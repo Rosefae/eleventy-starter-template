be6a8a4821b836034f1d5248c9af73e4e1c7f6e6
Rose Liu <rose@byanyothername.me>
2020-09-22 22:13:46 -0400

    Set up template

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+// Gulp only handles CSS stuff.
+// For everything else, please see .eleventy.js
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+
+sass.compiler = require('node-sass');
+
+var scss = function(cd){
+    return gulp.src('src/_styles/**/styles.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/_styles'));
+    cd();
+}
+
+var watchScss = function(cd){
+    gulp.watch('src/_styles/**/*.scss', { ignoreInitial:false }, scss);
+    cd();
+}
+
+exports.scss = scss;
+exports.default = watchScss;
